3b852c2fab024e75ea2e3795eb8dbf9a402ae7c5
Christian Jesús Travieso Alonso <christianjesustraviesoalonso@alumno.ieselrincon.es>
2026-01-29 19:40:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,11 @@ def comprobar():
     if caracter == "":
         return "<p style='color:red;'>No has escrito ningún carácter</p>"
 
+    # Comprobar si es letra
+    if not caracter.isalpha():
+        return f"<p style='color:orange; font-size:24px;'>{caracter} no es una letra</p>" \
+               "<a href='/'>Volver</a>"
+
     if caracter in vocales:
         return f"<p style='color:green; font-size:24px;'>{caracter.upper()} es una VOCAL</p>" \
                "<a href='/'>Volver</a>"
